5dbe0b75fed678933010446cf9991713ae40c090
Mr G. <131754080+RaymundGerardReyes@users.noreply.github.com>
2024-10-30 23:39:49 +0800

    Junier Gundaya

--- Divide.py
+++ Divide.py
@@ -1,11 +1,6 @@
 from typing import Union
 
-<<<<<<< HEAD
-def Multiply(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
-    Multiplies x by y and returns the result.
-    return x * y
-=======
 def Divide(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
     """Divides x by y and returns the result."""
     return x / y
->>>>>>> a6624e48a2153fd175021847daa88e8813601ccf
+
